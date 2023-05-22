20dafd194c3c011b7c6ede628b0ab0a091d44c30
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-05-22 12:33:27 +0100

    completed validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list
 
   validates :comment, length: { minimum: 6 }, allow_blank: true
-  validates :movie_id && :list_id, uniqueness: true
+  validates :movie, uniqueness: { scope: :list }
 end

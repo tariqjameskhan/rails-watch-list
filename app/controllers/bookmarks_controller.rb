90da73bc26ad2cbc6103e4b92401b284a7dc6fda
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-05-23 10:35:49 +0100

    added create bookmark feature

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,28 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
   def new
     @bookmark = Bookmark.new
+    # @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render new_list_bookmark_path(@list), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
     @list = List.find(params[:list_id])
   end
 end

6d9a384d25c39f8019df76a5ed84d9a907c2c4b2
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-05-24 20:17:33 +0100

    playing with Javascript

---
+++ app/javascript/controllers/tom_select_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select";
+
+// Connects to data-controller="tom-select"
+export default class extends Controller {
+  static values = { options: Object }
+
+  connect() {
+    new TomSelect(
+      this.element ,
+      this.optionsValue
+    );
+  }
+}
+
+// import { Controller } from "@hotwired/stimulus"
+// import TomSelect from "tom-select";
+
+// export default class extends Controller {
+//   connect() {
+//     new TomSelect(this.element)
+//   }
+// }
